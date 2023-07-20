f18941450f3443c8dcd106c304d0cc433dbae6cd
elvis <1255177148@qq.com>
2023-07-20 16:49:12 +0800

    坐标轴设置

--- 爬虫/双色球往期号码.py
+++ 爬虫/双色球往期号码.py
@@ -38,12 +38,12 @@ for data in resultList:
     # print(dateStr[0:index])
     x.append(dateStr[0:index])
     num1.append(int(code[0]))
-    num2.append(code[1])
-    num3.append(code[2])
-    num4.append(code[3])
-    num5.append(code[4])
-    num6.append(code[5])
-    num7.append(code[6])
+    num2.append(int(code[1]))
+    num3.append(int(code[2]))
+    num4.append(int(code[3]))
+    num5.append(int(code[4]))
+    num6.append(int(code[5]))
+    num7.append(int(code[6]))
 x.reverse()
 print(len(num1))
 print(len(x))
@@ -51,8 +51,18 @@ print(len(x))
 # 画折线图
 pylab.rcParams['font.sans-serif'] = ['SimHei'] # 指定默认字体
 pylab.rcParams['axes.unicode_minus'] = False # 显示负号，坐标轴上的负号显示
+pylab.ylim(0, 17)
+pylab.scatter(x, num7)
+'''
+默认画出的图，一般有四条轴，横坐标轴两条，分别是上(top)和下(buttom)，纵坐标轴两条，分别是左(left)和右(right)，
+现在我们想画出一般数学课上学的笛卡尔坐标系，只有两个坐标轴，那么就分别去掉横坐标的上(top)轴，和纵坐标的右(right)轴
+'''
+axes = pylab.gca() # 获取当前的坐标轴
+axes.spines['right'].set_color('none') # 右轴的颜色设置为空
+axes.spines['top'].set_color('none') # 上轴的颜色设置为空
+axes.spines['bottom'].set_position(('data', 0)) # 这里的data指的是纵坐标的值，也就是指将横坐标水平移动到纵坐标值为0的位置
+axes.spines['left'].set_position(('axes', 0.046)) # 这里的axes指的就是坐标轴，也就是指将纵坐标移到横坐标4.6%的位置
+axes
 pylab.xticks(rotation = 50)
-pylab.yticks(range(34))
-pylab.ylim(0, 33)
-pylab.scatter(x, num1)
+pylab.yticks(range(17))
 pylab.show()
