b877b3fe0309082cd5c0abec7fe8c6439613b5e9
elvis <1255177148@qq.com>
2024-09-30 17:02:18 +0800

    python基础

--- numpyDemo/demo1.py
+++ numpyDemo/demo1.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+'''
+numpy可以用来:
+1、数组的算数和逻辑运算;
+2、傅里叶变换和用于图形操作的例程;
+3、与线性代数有关的操作。numpy用于线性代数和随机数生成的内置函数
+'''
+
 a = np.arange(180).reshape(3, 5, 6, 2) # arange生成一个numpy数组
 
 print(a)
---
+++ 基础demo/封装.py
@@ -0,0 +1,16 @@
+class Person:
+    name = '测试人'
+    __age = 18 # 隐藏属性，前面用两个下划线声明这个是隐藏属性，子类不会集成隐藏属性，也不能被别的文件引用
+    _height = 180 # 私有属性，前面用一个下划线声明
+    
+    # 修改私有属性的方法
+    def setAge(self, age):
+        Person.__age = age
+    
+    # 私有方法
+    def _play(self):
+        print("玩游戏")
+
+p = Person()
+print(p._height)
+p._play()
