9c323e03ad935211a2fe8753495aeeef933207ae
elvis <1255177148@qq.com>
2024-10-02 17:58:16 +0800

    进程间的通信--使用队列

--- 基础demo/进程间的通信.py
+++ 基础demo/进程间的通信.py
@@ -1,6 +1,7 @@
 # 进程间的通信主要使用queue来通信
-from multiprocessing import Process
-from queue import Queue
+from multiprocessing import Process, Queue
+import time
+# from queue import Queue #这里的queue队列是用于多线程的，如果要用进程的队列，则在multiprocessing中导入
 
 # 初始化一个队列对象
 # q = Queue(3) # 最多可以接受3条消息，如果这里没有传参数或者传负数，则表示接受消息条数没有上限，直到耗尽内存
@@ -13,20 +14,17 @@ from queue import Queue
 def writeData(q1):
     for i in range(5):
         q1.put(i)
+        time.sleep(0.1)
 
 def readData(q2):
-    while True:
-        if q2.empty():
-            break
-        else:
-            print('取出的数据是：', q2.get())
+    while not q2.empty():
+        print('取出的数据是：', q2.get())
 
 if __name__ == '__main__':
     q = Queue()
-    q.put('一')
-    print(q.get())
     p1 = Process(target=writeData, args=(q,))
     p2 = Process(target=readData, args=(q,))
-    p1.start()()
+    p1.start()
+    p1.join()
     p2.start()
     p2.join()
