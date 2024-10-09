ab376eba345b98750e00454464b810033061f746
elvis <1255177148@qq.com>
2024-10-09 14:20:17 +0800

    python基础list

--- 基础demo/列表.py
+++ 基础demo/列表.py
@@ -3,6 +3,9 @@ li.append('four')
 print(li)
 li.extend(['five', 'six']) # 向列表追加一个新列表，也就是将另一个列表的所有元素分别加到原有列表里
 print(li)
+li.append(None)
+li.append(None) # 列表允许添加多个None
+print(li)
 
 for item in li:
     # comment: 
